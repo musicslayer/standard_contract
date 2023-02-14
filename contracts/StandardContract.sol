628c35f8f93f060862d2b7be0161815be50849de
musicslayer <musicslayer@gmail.com>
2023-02-14 13:12:17 -0500

    Add Base64 Library

--- contracts/StandardContract.sol
+++ contracts/StandardContract.sol
@@ -2,6 +2,46 @@
 
 pragma solidity 0.8.18;
 
+// import "@openzeppelin/contracts/utils/Base64.sol"
+library Base64 {
+    string internal constant _TABLE = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+    function encode(bytes memory data) internal pure returns (string memory) {
+        if(data.length == 0) return "";
+        string memory table = _TABLE;
+        string memory result = new string(4 * ((data.length + 2) / 3));
+        assembly {
+            let tablePtr := add(table, 1)
+            let resultPtr := add(result, 32)
+            for {
+                let dataPtr := data
+                let endPtr := add(data, mload(data))
+            } lt(dataPtr, endPtr) {
+            } {
+                // Advance 3 bytes
+                dataPtr := add(dataPtr, 3)
+                let input := mload(dataPtr)
+                mstore8(resultPtr, mload(add(tablePtr, and(shr(18, input), 0x3F))))
+                resultPtr := add(resultPtr, 1) // Advance
+                mstore8(resultPtr, mload(add(tablePtr, and(shr(12, input), 0x3F))))
+                resultPtr := add(resultPtr, 1) // Advance
+                mstore8(resultPtr, mload(add(tablePtr, and(shr(6, input), 0x3F))))
+                resultPtr := add(resultPtr, 1) // Advance
+                mstore8(resultPtr, mload(add(tablePtr, and(input, 0x3F))))
+                resultPtr := add(resultPtr, 1) // Advance
+            }
+            switch mod(mload(data), 3)
+            case 1 {
+                mstore8(sub(resultPtr, 1), 0x3d)
+                mstore8(sub(resultPtr, 2), 0x3d)
+            }
+            case 2 {
+                mstore8(sub(resultPtr, 1), 0x3d)
+            }
+        }
+        return result;
+    }
+}
+
 // import "@openzeppelin/contracts/token/ERC20/IERC20.sol"
 interface IERC20 {
     event Transfer(address indexed from, address indexed to, uint256 value);
